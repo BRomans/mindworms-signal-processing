e2a92d9b1ac2d747c5c82d29e535ddfb9fe46219
BRomans <michele.romani.gzl0@gmail.com>
2021-07-07 15:38:13 +0200

    :sparkles: implemented parametrized number of channels for generated sample and the possibility to choose in which channel are beta and smr

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,30 +1,32 @@
-from utils.json_writer import JsonWriter
 from pylsl import StreamInfo, StreamOutlet, cf_float32
+import numpy as np
 import json
 import time
 
 
 class DataGenerator:
 
-    def __init__(self, filename):
-        info = StreamInfo('type', 'EEG', 32, 0, cf_float32, 'test')
+    def __init__(self, filename, n_channels=32):
+        info = StreamInfo('type', 'EEG', n_channels, 0, cf_float32, 'test')
+        self.n_channels = n_channels
         self.outlet = StreamOutlet(info)
         file = open(filename, )
         self.json_source = json.load(file)
 
     ''' Generate a stream of zero values except on channel 9 (SMR) and channel 23 (Beta) '''
-    def generate_stream(self, delay=0):
+    def generate_stream(self, delay=0.0, idx_beta=9, idx_smr=22):
         while True:
             for sample in self.json_source['stream']:
-                f_sample = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,  0.0, 0.0, float(sample[0]), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, float(sample[1]), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]  # Convert string values to float
-                self.outlet.push_sample(f_sample)
-                print("Sending: ", f_sample)
+                f_sample = np.zeros(self.n_channels)  # Convert string values to float
+                f_sample[idx_beta] = float(sample[0])
+                f_sample[idx_smr] = float(sample[1])
+                self.outlet.push_sample(f_sample.tolist())
+                print("Sending: ", list(f_sample))
                 time.sleep(delay)
 
 
 generator = DataGenerator('../data/20210628-103050_mindworms_recording.json')
-generator.generate_stream()
+generator.generate_stream(0.25, 9, 31)
 
 
 
