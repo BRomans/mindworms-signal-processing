d0c250199d6b64eb49ef5c961af240631ce1d862
BRomans <michele.romani.gzl0@gmail.com>
2021-07-07 16:09:53 +0200

    :sparkles: function to convert datasets without line separators [WIP]

--- utils/json_writer.py
+++ utils/json_writer.py
@@ -6,9 +6,9 @@ FOLDER = '../data'
 
 class JsonWriter:
 
-    def __init__(self):
+    def __init__(self, user='test'):
         self.data = {'stream': [], 'smr': [], 'beta': []}
-        self.filename = str(datetime.now().strftime("%Y%m%d-%H%M%S")) + '_mindworms_recording'
+        self.filename = user + '_' + str(datetime.now().strftime("%Y%m%d-%H%M%S")) + '_mindworms_recording'
 
     ''' Add sample to the dictionary'''
     def add_sample(self, smr, beta):
@@ -30,4 +30,25 @@ class JsonWriter:
                     self.add_sample(smr, beta)
                     print(smr, beta)
             self.write_file()
+
+    ''' Convert old .txt format into json'''
+
+    def convert_textfile_single_line(self, file):
+        with open(file + '.txt', 'r') as file:
+            for line in file:
+                for word in line.split():
+                    counter = 0
+                    if word.strip():
+                        smr = ''
+                        beta = ''
+                        if counter == 0:
+                            smr = word
+                            counter += 1
+                        if counter == 1:
+                            beta = word
+                            counter -= 1
+                        self.add_sample(smr, beta)
+                        print(str(smr), str(beta))
+            self.write_file()
+
 
